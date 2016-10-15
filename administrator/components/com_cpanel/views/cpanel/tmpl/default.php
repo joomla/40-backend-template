c96f3991b71df1baf930dd5b80186b8430ce8a68
Dimitris <d.grammatiko@gmail.com>
2016-10-15 20:59:57 +0300

    drag n drop c-panel

--- administrator/components/com_cpanel/views/cpanel/tmpl/default.php
+++ administrator/components/com_cpanel/views/cpanel/tmpl/default.php
@@ -12,6 +12,21 @@ defined('_JEXEC') or die;
 use Joomla\Registry\Registry;
 
 $user = JFactory::getUser();
+JHtml::_('script', 'vendor/dragula/dragula.min.js', false, true);
+JHtml::_('stylesheet', 'vendor/dragula/dragula.min.css', array(), true);
+
+JFactory::getDocument()->addScriptDeclaration(
+<<<JS
+		document.addEventListener('DOMContentLoaded', function() {
+
+	var container = document.querySelector('.js-draggable');
+
+	var sortableTable = dragula([container]);
+
+});
+
+JS
+);
 ?>
 <div class="row">
 	<?php $iconmodules = JModuleHelper::getModules('icon');
@@ -28,30 +43,29 @@ $user = JFactory::getUser();
 			</div>
 		</div>
 	<?php endif; ?>
-	<div class="col-md-<?php echo ($iconmodules) ? 9 : 12; ?>">
-		<?php if ($user->authorise('core.manage', 'com_postinstall') && $this->postinstall_message_count) : ?>
-			<div class="row">
-				<div class="col-md-12">
-					<div class="alert alert-info">
-						<h4>
-							<?php echo JText::_('COM_CPANEL_MESSAGES_TITLE'); ?>
-						</h4>
-						<p>
-							<?php echo JText::_('COM_CPANEL_MESSAGES_BODY_NOCLOSE'); ?>
-						</p>
-						<p>
-							<?php echo JText::_('COM_CPANEL_MESSAGES_BODYMORE_NOCLOSE'); ?>
-						</p>
-						<p>
-							<a href="index.php?option=com_postinstall&amp;eid=700" class="btn btn-primary">
-								<?php echo JText::_('COM_CPANEL_MESSAGES_REVIEW'); ?>
-							</a>
-						</p>
-					</div>
+	<?php if ($user->authorise('core.manage', 'com_postinstall') && $this->postinstall_message_count) : ?>
+		<div class="row">
+			<div class="col-md-12">
+				<div class="alert alert-info">
+					<h4>
+						<?php echo JText::_('COM_CPANEL_MESSAGES_TITLE'); ?>
+					</h4>
+					<p>
+						<?php echo JText::_('COM_CPANEL_MESSAGES_BODY_NOCLOSE'); ?>
+					</p>
+					<p>
+						<?php echo JText::_('COM_CPANEL_MESSAGES_BODYMORE_NOCLOSE'); ?>
+					</p>
+					<p>
+						<a href="index.php?option=com_postinstall&amp;eid=700" class="btn btn-primary">
+							<?php echo JText::_('COM_CPANEL_MESSAGES_REVIEW'); ?>
+						</a>
+					</p>
 				</div>
 			</div>
-		<?php endif; ?>
-		<div class="row">
+		</div>
+	<?php endif; ?>
+	<div class="js-draggable col-md-<?php echo ($iconmodules) ? 9 : 12; ?>">
 			<?php
 			$cols = 0;
 
@@ -62,16 +76,8 @@ $user = JFactory::getUser();
 				$params->loadString($module->params);
 				$bootstrapSize = $params->get('bootstrap_size', 6);
 
-				$cols += $bootstrapSize;
-				if ($cols > 12)
-				{
-					echo '</div><div class="row">';
-					$cols = $bootstrapSize;
-				}
-
 				echo JModuleHelper::renderModule($module, array('style' => 'well'));
 			}
 			?>
-		</div>
 	</div>
 </div>

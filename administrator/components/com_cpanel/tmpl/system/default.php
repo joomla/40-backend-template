550584a480d6eb73d4bc994a99dfacfa90cab9fb
ciar4n <ciaran@joomla51.com>
2018-07-01 18:19:59 +0100

    Style help and system pages

--- administrator/components/com_cpanel/tmpl/system/default.php
+++ administrator/components/com_cpanel/tmpl/system/default.php
@@ -10,19 +10,19 @@
 defined('_JEXEC') or die;
 ?>
 
-<?php foreach ($this->links as $name => $links) : ?>
-<h3><?php echo \Joomla\CMS\Language\Text::_($name); ?></h3>
-<div class="card-columns">
-	<?php foreach ($links as $id => $link) : ?>
-		<div class="card">
-			<div class="card-block">
-				<h4 class="card-title"><?php echo JText::_($link['title']); ?></h4>
-				<span class="fa fa-<?php echo $link['icon']; ?> fa-5x"></span>
-				<p class="card-text"><?php echo JText::_($link['title']); ?></p>
-				<a href="<?php echo $link['link']; ?>" class="btn btn-primary"><?php echo JText::_($link['title']); ?></a>
-			</div>
-		</div>
+<div class="com-cpanel-system">
+	<?php foreach ($this->links as $name => $links) : ?>
+	<div class="com-cpanel-system__category">
+		<h4 class="com-cpanel-system__header"><span class="fa fa-<?php echo $link['icon']; ?>"></span><?php echo \Joomla\CMS\Language\Text::_($name); ?></h4>
+		<ul class="list-group list-group-flush">
+			<?php foreach ($links as $id => $link) : ?>
+				<li class="list-group-item">
+					<a href="<?php echo $link['link']; ?>"><?php echo JText::_($link['title']); ?></a>
+				</li>
+			<?php endforeach; ?>
+		</ul>
+	</div>
 	<?php endforeach; ?>
 </div>
-<hr>
-<?php endforeach; ?>
+
+
